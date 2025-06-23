c4d02b8c44900c6c471768279ccdd6b42631b87d
Abhishek Singh <rathoreabhishek006@gmail.com>
2025-06-24 02:29:45 +0530

    initial commit

---
+++ jsfiles/data.js
@@ -0,0 +1,113 @@
+const projData = [
+  {
+    image: "assets/1155.jpg",
+    title: "Project 1",
+    subHeading: "HTML / CSS",
+    description: "A simple project.",
+  },
+  {
+    image: "images/project2.jpg",
+    title: "Project 2",
+    subHeading: "JavaScript",
+    description: "Another great build.",
+  },
+  {
+    image: "images/project2.jpg",
+    title: "Project 2",
+    subHeading: "JavaScript",
+    description: "Another great build.",
+  },
+  {
+    image: "images/project2.jpg",
+    title: "Project 2",
+    subHeading: "JavaScript",
+    description: "Another great build.",
+  },
+  {
+    image: "images/project2.jpg",
+    title: "Project 2",
+    subHeading: "JavaScript",
+    description: "Another great build.",
+  },
+  {
+    image: "images/project2.jpg",
+    title: "Project 2",
+    subHeading: "JavaScript",
+    description: "Another great build.",
+  },
+  {
+    image: "images/project2.jpg",
+    title: "Project 2",
+    subHeading: "JavaScript",
+    description: "Another great build.",
+  },
+  {
+    image: "images/project2.jpg",
+    title: "Project 2",
+    subHeading: "JavaScript",
+    description: "Another great build.",
+  },
+];
+
+const skillData = [
+  {
+    img: "assets/img/htmlImg.png",
+    h4: "HTML",
+    progressValue: "90",
+  },
+  {
+    img: "assets/img/cssImg.svg",
+    h4: "CSS",
+    progressValue: "85",
+  },
+  {
+    img: "assets/img/jsImg.png",
+    h4: "JavaScript",
+    progressValue: "80",
+  },
+  {
+    img: "assets/img/mysqlImg.avif",
+    h4: "MySQL",
+    progressValue: "70",
+  },
+  {
+    img: "assets/img/msofficeImg.webp",
+    h4: "MS Office",
+    progressValue: "100",
+  },
+  {
+    img: "assets/img/gsapImg.png",
+    h4: "GSAP JS",
+    progressValue: "80",
+  },
+  {
+    img: "assets/img/gitImg.png",
+    h4: "Git Hub",
+    progressValue: "60",
+  },
+  {
+    img: "assets/img/reactImg.png",
+    h4: "React",
+    progressValue: "5",
+  },
+  {
+    img: "assets/img/nodeImg.png",
+    h4: "Node JS",
+    progressValue: "0",
+  },
+  {
+    img: "assets/img/expressImg.webp",
+    h4: "Express JS",
+    progressValue: "0",
+  },
+  {
+    img: "assets/img/mongoImg.png",
+    h4: "Mongo DB",
+    progressValue: "5",
+  },
+  {
+    img: "assets/img/cppimg.png",
+    h4: "C++",
+    progressValue: "50",
+  },
+];
---
+++ jsfiles/javascript.js
@@ -0,0 +1,77 @@
+//portfolio js start
+projData.forEach((p, i) => {
+  let card_inner = document.createElement("div");
+  card_inner.classList.add("card-inner");
+
+  let card_front = document.createElement("div");
+  card_front.classList.add("card-front");
+
+  let card_img = document.createElement("img");
+  card_img.src = p.image; // assuming p.image contains the image URL
+
+  let card_back = document.createElement("div");
+  card_back.classList.add("card-back");
+
+  let card_proTitle = document.createElement("h1");
+  card_proTitle.classList.add("proTitle");
+  card_proTitle.textContent = p.title;
+
+  let card_subHeading = document.createElement("h4");
+  card_subHeading.textContent = p.subHeading;
+
+  let card_para = document.createElement("p");
+  card_para.textContent = p.description;
+
+  let card_button = document.createElement("button");
+  card_button.textContent = "View Project";
+
+  // Append structure as needed
+  card_front.appendChild(card_img);
+  card_back.appendChild(card_proTitle);
+  card_back.appendChild(card_subHeading);
+  card_back.appendChild(card_para);
+  card_back.appendChild(card_button);
+
+  card_inner.appendChild(card_front);
+  card_inner.appendChild(card_back);
+
+  let card = document.createElement("div");
+  card.classList.add("proj");
+  card.appendChild(card_inner);
+
+  document.querySelector(".projectContent").appendChild(card); // change container selector as needed
+});
+
+// portfolio card hover effect
+document.querySelectorAll(".proj").forEach((proj) => {
+  let cardInner = proj.querySelector(".card-inner");
+
+  proj.addEventListener("mouseenter", () => {
+    gsap.to(cardInner, { rotationY: 180, duration: 0.6, ease: "power2.out" });
+  });
+
+  proj.addEventListener("mouseleave", () => {
+    gsap.to(cardInner, { rotationY: 0, duration: 0.6, ease: "power2.out" });
+  });
+});
+//portfolio js end
+//skill js start
+skillData.forEach((p, i) => {
+  let skill_container = document.querySelector(".skill-container");
+  console.log(p, i);
+  const skillBox = document.createElement("div");
+  skillBox.classList.add("skill-box");
+  const skillBox_img = document.createElement("img");
+  skillBox_img.src = p.img;
+  const skillBox_h4 = document.createElement("h4");
+  skillBox_h4.innerText = p.h4;
+  const skillBox_progress = document.createElement("progress");
+  skillBox_progress.classList.add("progress");
+  skillBox_progress.max = "100";
+  skillBox_progress.value = p.progressValue;
+  skillBox.appendChild(skillBox_img);
+  skillBox.appendChild(skillBox_h4);
+  skillBox.appendChild(skillBox_progress);
+  skill_container.appendChild(skillBox);
+});
+//skill js end
